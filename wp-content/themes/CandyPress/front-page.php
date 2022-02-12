238d192dc61bf0e83b6ec39b4da777642ef28f63
Master0fDrapes <akshaymuralidhara@outlook.com>
2022-02-12 19:38:54 +0530

    home page changes

--- wp-content/themes/CandyPress/front-page.php
+++ wp-content/themes/CandyPress/front-page.php
@@ -1,4 +1,11 @@
 <?php
+	/**
+	 * The template for displaying Home Page
+	 *
+	 * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+	 *
+	 * @package CandyPress
+	 */
 	get_header();
 	$t_options = get_option('tp_opt');
 	global $tempDir;
